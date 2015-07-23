3511c6844344fed4943ec318f0a952c3d47e72d4
Rui Xu <xurui@mcmaster.ca>
2015-07-23 15:25:51 -0400

    Initial Commit

---
+++ SCFT_1D_July23/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SCFT_1D_July23
+//
+//  Created by Rui Xu on 2015-07-23.
+//  Copyright (c) 2015 McMaster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
